2dc06f0497aa119518f17e81240072dc8f81f881
PauloAraujoDEV25 <118487008+PauloAraujoDEV25@users.noreply.github.com>
2023-03-08 12:16:58 -0300

    att estudos

---
+++ ex006/aula011.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`A velocidade do seu carro é ${vel} km/h.`)
+if(vel > 60){
+console.log('você ultrapassou a velocidade permitida.Multado!')
+}else
+console.log('dirija sempre usando cinto de segurança.')
